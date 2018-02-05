8bee7e2477fafeb7445e1c9b7ee9acb66436336f
Sawyer <870756077@qq.com>
2018-02-05 18:34:53 +0800

    update

--- App.js
+++ App.js
@@ -3,12 +3,12 @@
 // import DrawerNavigator from './DrawerNavigator'
 // import NavigationActions from './NavigationActions'
 
-export { default as ReduxExample } from './ReduxExample'
+import ReduxExample from './ReduxExample'
 
 // import App from './react-navive-dva-antd'
 // import ReduxExample from './ReduxExampleDemo'
 
-// export default ReduxExample
+export default ReduxExample
 // export default StackNavigator
 
 // export default App
